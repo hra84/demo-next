1ac1ee9c340d5132efaed27cbbef403ed7401287
hra84 <106070884+hra84@users.noreply.github.com>
2022-07-05 00:54:55 -1200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,7 +17,7 @@ class MyDocument extends Document {
             name="description"
             content="Stora - React Ecommerce Template with NextJs, AntDesign, ReactHooks"
           />
-          <link rel="icon" href="/fav.png" />
+          <link rel="icon" href="https://i.postimg.cc/g0kDYYYx/RD-Premade-Logo-Core-Logo-Yesq-Arts-removebg.png" />
           <link
             href="https://kit-pro.fontawesome.com/releases/v5.13.0/css/pro.min.css"
             rel="stylesheet"
